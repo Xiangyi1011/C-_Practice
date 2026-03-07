aed5221393ff8a05b1ce72048300bcf4e29530f8
Xiangy <enerty1011@gmail.com>
2026-03-07 08:03:46 +0000

    利用指標變數將無解改為有解

--- function_2.cpp
+++ function_2.cpp
@@ -1,19 +1,20 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// //兩數對調
-// void swap(int n, int m){ //因為call by value, 所以無解
-//   int temp;
-//   temp = n;
-//   n = m;
-//   m = temp;
-// }
+//兩數對調
+void swap(int *n, int *m){ //因為call by address, 先copy位址, 所以不會消失
+  //取到值後開始改變main裡的值
+  int temp;
+  temp = *n;
+  *n = *m;
+  *m = temp;
+}
 
-// //兩數對調
-//   int main(){
-//   int x = 666;
-//   int y = 888;
-//   cout << "x=" << x << ",y=" << y << '\n';
-//   swap(x,y);
-//   cout << "x=" << x <<   ",y=" << y << '\n';
-// }
+//兩數對調
+  int main(){
+  int x = 666;
+  int y = 888;
+  cout << "x=" << x << ",y=" << y << '\n';
+  swap(&x,&y); //取位址
+  cout << "x=" << x <<   ",y=" << y << '\n';
+}
